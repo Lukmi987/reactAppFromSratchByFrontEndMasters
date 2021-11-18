f9615ae45a9b432d1aa612a9ee96e0e33fd97baf
Lukas <>
2021-11-18 20:24:02 +0100

    adding prettier and extension to vscode

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # reactAppFromSratchByFrontEndMasters
+
     // Every time that you create a new tag or you are creating
     // a new instance of the component, you have to use React.createElement
 
     Data flows down, one way data flow
+
+---
+
+NPM
+
+---
+
+ctrl + , :settings in Vs code then
+write: prettier config: Prettier: Require Config it only will run for projects where is config
--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2",{}, props.name),
-        React.createElement("h3",{}, props.animal),
-        React.createElement("h4",{}, props.breed),
-            ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h4", {}, props.breed),
+  ]);
+};
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-        React.createElement("h1", {}, "Adopt Me!!"),
-        React.createElement(Pet, {name: "Luna", animal: "dog", breed: "havaniis"}),
-        React.createElement(Pet, {name: "Peeper", animal: "Bird", breed: "havaniis"}),
-        React.createElement(Pet, {name: "Sudo", animal: "Crodile", breed: "havaniis"}),
-    ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "havaniis",
+    }),
+    React.createElement(Pet, {
+      name: "Peeper",
+      animal: "Bird",
+      breed: "havaniis",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Crodile",
+      breed: "havaniis",
+    }),
+  ]);
 };
 
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
