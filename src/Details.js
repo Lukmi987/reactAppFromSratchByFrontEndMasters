6998649fc4046d499e1b15f22dbcd121160a7626
Lukas <komprs.l@gmail.com>
2021-11-28 21:44:58 +0100

    Using modal which lives outside of root component

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
+import ThemeContext from "./ThemeContext";
 
 const Details = () => {
-  return <h2>hi loljk wth</h2>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isShowModal, setIsShowModal] = useState(false);
+
+  const toggleModal = () => setIsShowModal(!isShowModal);
+  const adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
+  return (
+    <div>
+      <ThemeContext.Consumer>
+        {([theme]) => (
+          <button onClick={toggleModal} style={{ backgroundColor: theme }}>
+            Adopt
+          </button>
+        )}
+      </ThemeContext.Consumer>
+      {isShowModal ? (
+        // Modal lives outside of root
+        <Modal>
+          <div>
+            <h1>Wou you like to adopt me</h1>
+            <div className="buttons">
+              <button onClick={adopt}>Yes</button>
+              <button onClick={toggleModal}>NO</button>
+            </div>
+          </div>
+        </Modal>
+      ) : // if we give react null do not render anything
+      null}
+    </div>
+  );
 };
 
 export default Details;
